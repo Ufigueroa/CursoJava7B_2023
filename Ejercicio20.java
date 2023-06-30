26065503369be496ff833263b2daccf460e71397
Ufigueroa <138156555+Ufigueroa@users.noreply.github.com>
2023-06-29 22:51:13 -0300

    Update and rename Ejercicio 20 to Ejercicio20.java

--- Ejercicio20.java
+++ Ejercicio20.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 
-public class RandomNumbers {
+public class Ejercicio20 {
     public static void main(String[] args) {
         Random random = new Random();
         int count = 0;
