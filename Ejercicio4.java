699b35fd96181288ea339714413c56361aa417f5
Ufigueroa <138156555+Ufigueroa@users.noreply.github.com>
2023-06-29 22:27:44 -0300

    Update Ejercicio4.java

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -3,7 +3,7 @@ Ejercicio 4:
 
 import java.util.Scanner;
 
-public class ejercicio4 {
+public class Ejercicio4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
