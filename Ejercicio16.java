6a2c42af6b24bdb36c2aa24139c0601d789cf749
Ufigueroa <138156555+Ufigueroa@users.noreply.github.com>
2023-06-29 22:44:32 -0300

    Update and rename Ejercicio 16 to Ejercicio16.java

--- Ejercicio16.java
+++ Ejercicio16.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
-public class MultiplicationTable {
+public class Ejercicio16 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter a number: ");
+        System.out.print("Ingrese un número: ");
         int number = scanner.nextInt();
 
-        System.out.println("Multiplication Table for " + number + ":");
+        System.out.println("Tabla de multiplicar para " + number + ":");
 
         for (int i = 1; i <= 10; i++) {
             int result = number * i;
