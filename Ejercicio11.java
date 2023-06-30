250846bbb55f09baaa831fe9f70dead8597413fc
Ufigueroa <138156555+Ufigueroa@users.noreply.github.com>
2023-06-29 22:33:04 -0300

    Update and rename Ejercicio-11 to Ejercicio11.java

--- Ejercicio11.java
+++ Ejercicio11.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
 
-public class VowelCheck {
+public class Ejercicio11 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter a letter: ");
+        System.out.println("Ingrese una letra: ");
         char letter = scanner.nextLine().charAt(0);
 
         boolean isVowel = (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u' ||
                            letter == 'A' || letter == 'E' || letter == 'I' || letter == 'O' || letter == 'U');
 
         if (isVowel) {
-            System.out.println("The letter is a vowel.");
+            System.out.println("La letra es una vocal.");
         } else {
-            System.out.println("The letter is not a vowel.");
+            System.out.println("La letra no es una vocal.");
         }
 
         scanner.close();
